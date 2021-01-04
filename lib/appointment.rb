42c5999a4cb074f7b0c78db284d6385171cb51eb
Janis Lebron <janislebron@gmail.com>
2021-01-04 23:08:59 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,7 +3,7 @@ class Appointment
   attr_accessor :date, :patient, :doctor
   @@all = []
 
-  def initialize(date ="", doctor = "", patient = "")
+  def initialize(date ="", patient = "", patient = "")
     @date = date
     @doctor = doctor
     @patient = patient
